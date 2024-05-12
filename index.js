5dc8ffa07d76ac4713cee85813e36a11621667f5
Durga Sumesh <durgasumesh17@gmail.com>
2024-05-12 14:49:55 +0530

    add edit functionality

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 let posts = [];
+let indexToEdit;
 
 app.get("/", (req, res) => {
     res.render("index.ejs", {posts});
@@ -21,9 +22,12 @@ app.post("/", (req, res) => {
     res.redirect('/');
 });
 
-app.get("/create", (req, res) => {
-    res.render("create.ejs");
-});
+app.post("/edited", (req, res) => {
+    const {title, content} = req.body;
+    const newPost = {title, content};
+    posts[indexToEdit - 1] = newPost;
+    res.redirect('/');
+})
 
 app.post("/create", (req, res) => {
     const { title, content } = req.body;
@@ -31,6 +35,23 @@ app.post("/create", (req, res) => {
     posts.push(newPost);
     res.redirect('/');
 });
+
+app.post("/edit", (req, res) => {
+    indexToEdit = req.body.m;
+    console.log(posts[indexToEdit - 1]);
+    const t = posts[indexToEdit - 1].title;
+    const c = posts[indexToEdit - 1].content;
+    res.render("edit.ejs", {
+        title : t,
+        content : c
+    })
+})
+
+app.get("/create", (req, res) => {
+    res.render("create.ejs");
+});
+
+
 
 app.listen(port, () => {
     console.log(`listening at port ${port}`);
